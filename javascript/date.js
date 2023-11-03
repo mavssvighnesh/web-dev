e6d8a341dbeaa8fd76b1a7a229201339fa833e8b
mavssvighnesh <109013315+mavssvighnesh@users.noreply.github.com>
2023-11-03 21:44:47 +0530

    demonstrate the date method

---
+++ javascript/date.js
@@ -0,0 +1,12 @@
+//date 
+
+var currentdate=new Date();  //using the date method to get the todays date 
+console.log(currentdate); //printing the current date and time of the machine 
+
+var date=new Date();
+date.setDate(23); //setting the date to 23 using the set date method 
+date.setMonth(12);  //setting the month to the januaary using the set month method 
+date.setFullYear(2004);  //setting the year to 20044 using the set year method 
+console.log(date); //printing the date 
+
+
