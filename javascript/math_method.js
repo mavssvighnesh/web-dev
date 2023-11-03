fe58490d4fedf15da4cc80e762b69dc213b233f3
mavssvighnesh <109013315+mavssvighnesh@users.noreply.github.com>
2023-11-03 21:45:13 +0530

    math method usage in js

---
+++ javascript/math_method.js
@@ -0,0 +1,11 @@
+//the math module in the js 
+
+console.log(Math.floor(3.8)); //prints the floor value of the 3.8 =3
+
+console.log(Math.ceil(3.2)); //prints the ceiling value of 3.2
+
+console.log(Math.max(1,56,23,88,9,55,100)); //prints the maximum element out of the list 
+
+console.log(Math.min(1,56,23,88,9,55,100));//prints the minimum element out of the list 
+
+console.log(Math.random()); //prints a random number 
